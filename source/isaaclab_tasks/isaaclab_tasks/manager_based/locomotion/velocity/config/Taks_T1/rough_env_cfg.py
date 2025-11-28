8ee7cd2a1c221eaeafa58c623443827e3c81db52
AIResearcherHZ <xhz2082416211@gmail.com>
2025-11-28 20:31:17 +0800

    优化奖励权重

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/Taks_T1/rough_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/Taks_T1/rough_env_cfg.py
@@ -20,14 +20,14 @@ class TaksT1Rewards(RewardsCfg):
     # 追踪线速度奖励
     track_lin_vel_xy_exp = RewTerm(
         func=mdp.track_lin_vel_xy_yaw_frame_exp,
-        weight=2.0,
+        weight=1.0,
         params={"command_name": "base_velocity", "std": 0.5},
     )
 
     # 追踪角速度奖励
     track_ang_vel_z_exp = RewTerm(
         func=mdp.track_ang_vel_z_world_exp,
-        weight=2.0,
+        weight=1.0,
         params={"command_name": "base_velocity", "std": 0.5},
     )
 
