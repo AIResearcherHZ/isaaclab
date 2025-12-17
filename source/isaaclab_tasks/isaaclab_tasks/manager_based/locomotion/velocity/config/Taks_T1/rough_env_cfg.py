7316af84554c9a00530ce71df771793ce516a4f7
AIResearcherHZ <xhz2082416211@gmail.com>
2025-12-17 16:44:47 +0800

    删除Taks T1机器人URDF文件:移除包含完整机器人定义的1052行URDF文件,包括骨盆、双腿、腰部、躯干、双臂、颈部和头部的所有link和joint定义及其惯性参数、视觉和碰撞几何体

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/Taks_T1/rough_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/Taks_T1/rough_env_cfg.py
@@ -51,21 +51,21 @@ class TaksT1Rewards(RewardsCfg):
     # 颈部关节偏差惩罚 - 保持头部稳定
     joint_deviation_neck = RewTerm(
         func=mdp.joint_deviation_l1,
-        weight=-0.1,
+        weight=-0.25,
         params={"asset_cfg": SceneEntityCfg("robot", joint_names=["neck_.*"])},
     )
 
     # 腰部偏差惩罚：抑制躯干晃动，保持腰部姿态稳定
     joint_deviation_torso = RewTerm(
         func=mdp.joint_deviation_l1,
-        weight=-0.2,
+        weight=-0.25,
         params={"asset_cfg": SceneEntityCfg("robot", joint_names=["waist_pitch_joint", "waist_yaw_joint", "waist_roll_joint"])},
     )
 
     # 手臂关节偏差惩罚：减少上肢多余摆动，保持动作干净
     joint_deviation_arms = RewTerm(
         func=mdp.joint_deviation_l1,
-        weight=-0.2,
+        weight=-0.3,
         params={
             "asset_cfg": SceneEntityCfg(
                 "robot",
@@ -151,46 +151,46 @@ class TaksT1Rewards(RewardsCfg):
         },
     )
 
-    # 条件双脚同时接触惩罚：仅有指令时惩罚
-    double_support_penalty_cond = RewTerm(
-        func=mdp.double_support_time_penalty_conditional,
-        weight=-2.5,
-        params={
-            "command_name": "base_velocity",
-            "command_threshold": 0.1,
-            "sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*_ankle_roll_link"),
-            "max_double_support_time": 0.2,
-        },
-    )
+    # # 条件双脚同时接触惩罚：仅有指令时惩罚
+    # double_support_penalty_cond = RewTerm(
+    #     func=mdp.double_support_time_penalty_conditional,
+    #     weight=-2.5,
+    #     params={
+    #         "command_name": "base_velocity",
+    #         "command_threshold": 0.1,
+    #         "sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*_ankle_roll_link"),
+    #         "max_double_support_time": 0.2,
+    #     },
+    # )
 
-    # 条件单脚支撑奖励：仅有指令时奖励
-    single_leg_stance_cond = RewTerm(
-        func=mdp.single_leg_stance_reward_conditional,
-        weight=0.1,
-        params={
-            "command_name": "base_velocity",
-            "command_threshold": 0.1,
-            "sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*_ankle_roll_link"),
-        },
-    )
+    # # 条件单脚支撑奖励：仅有指令时奖励
+    # single_leg_stance_cond = RewTerm(
+    #     func=mdp.single_leg_stance_reward_conditional,
+    #     weight=0.1,
+    #     params={
+    #         "command_name": "base_velocity",
+    #         "command_threshold": 0.1,
+    #         "sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*_ankle_roll_link"),
+    #     },
+    # )
 
-    # 条件双脚交替接触奖励：仅有指令时奖励
-    feet_alternating_cond = RewTerm(
-        func=mdp.feet_alternating_contact_conditional,
-        weight=0.05,
-        params={
-            "command_name": "base_velocity",
-            "command_threshold": 0.1,
-            "sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*_ankle_roll_link"),
-        },
-    )
+    # # 条件双脚交替接触奖励：仅有指令时奖励
+    # feet_alternating_cond = RewTerm(
+    #     func=mdp.feet_alternating_contact_conditional,
+    #     weight=0.05,
+    #     params={
+    #         "command_name": "base_velocity",
+    #         "command_threshold": 0.1,
+    #         "sensor_cfg": SceneEntityCfg("contact_forces", body_names=".*_ankle_roll_link"),
+    #     },
+    # )
 
-    # 条件速度方向对齐奖励：仅有指令时奖励
-    velocity_alignment_cond = RewTerm(
-        func=mdp.velocity_direction_alignment_conditional,
-        weight=0.05,
-        params={"command_name": "base_velocity", "command_threshold": 0.1},
-    )
+    # # 条件速度方向对齐奖励：仅有指令时奖励
+    # velocity_alignment_cond = RewTerm(
+    #     func=mdp.velocity_direction_alignment_conditional,
+    #     weight=0.05,
+    #     params={"command_name": "base_velocity", "command_threshold": 0.1},
+    # )
 
     # 条件动作变化率惩罚：仅有指令时惩罚，无指令时允许自由调整以保持平衡
     action_rate_l2_cond = RewTerm(
