d1ce835475e584d3c9a9a1ea84c658c3418f2832
AIResearcherHZ <xhz2082416211@gmail.com>
2025-12-24 11:05:15 +0800

    提升Taks T1机器人Rough环境颈部偏差惩罚权重从-0.5至-0.75并扩大环境间距从3.5至5.0:强化颈部稳定性控制以改善头部姿态,扩大环境间距以优化空间利用和避免环境间干扰

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
@@ -404,7 +404,7 @@ class LocomotionVelocityRoughEnvCfg(ManagerBasedRLEnvCfg):
     """
 
     # 场景设置：创建 MySceneCfg 实例并设置并行环境数量与环境间距
-    scene: MySceneCfg = MySceneCfg(num_envs=4096, env_spacing=3.5)
+    scene: MySceneCfg = MySceneCfg(num_envs=4096, env_spacing=5.0)
     # 观察、动作、命令配置
     observations: ObservationsCfg = ObservationsCfg()
     actions: ActionsCfg = ActionsCfg()
